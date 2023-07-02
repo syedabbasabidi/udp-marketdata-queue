a570d2f079022b2d870bb36e12863b733ac325b5
Abbas <monazaidi5@gmail.com>
2023-07-02 21:40:23 +0100

    lowlat-4 udp benchmark

---
+++ src/test/java/com/abidi/perf/JLBHTCPConsumerBroker.java
@@ -0,0 +1,63 @@
+package com.abidi.perf;
+
+import com.abidi.broker.UDPConsumerBroker;
+import com.abidi.broker.UDPProducerBroker;
+import com.abidi.consumer.UDPQueueConsumer;
+import com.abidi.producer.UDPQueueProducer;
+import net.openhft.chronicle.jlbh.JLBH;
+import net.openhft.chronicle.jlbh.JLBHOptions;
+import net.openhft.chronicle.jlbh.JLBHTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static java.lang.System.nanoTime;
+
+public class JLBHTCPConsumerBroker implements JLBHTask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JLBHTCPConsumerBroker.class);
+
+    private JLBH jlbh;
+    private UDPProducerBroker udpProducerBroker;
+    private UDPQueueConsumer udpQueueConsumer;
+    private UDPConsumerBroker udpConsumerBroker;
+    private UDPQueueProducer udpQueueProducer;
+
+
+    public static void main(String[] args) {
+        JLBHOptions jlbhOptions = new JLBHOptions()
+                .warmUpIterations(10_000).iterations(5_000_000).throughput(1_000_000).runs(3)
+                .accountForCoordinatedOmission(false).recordOSJitter(false).jlbhTask(new JLBHTCPConsumerBroker());
+
+        new JLBH(jlbhOptions).start();
+    }
+
+    @Override
+    public void init(JLBH jlbh) {
+        this.jlbh = jlbh;
+        try {
+            udpConsumerBroker = new UDPConsumerBroker();
+            udpProducerBroker = new UDPProducerBroker();
+            udpQueueConsumer = new UDPQueueConsumer();
+            udpQueueProducer = new UDPQueueProducer();
+            initialize();
+        } catch (IOException exp) {
+            LOG.error("Failed to initialize JLBH", exp);
+        }
+    }
+
+    @Override
+    public void run(long startTimeNS) {
+        udpConsumerBroker.getNextAndAck();
+        jlbh.sampleNanos((nanoTime() - 10) - startTimeNS);
+    }
+
+
+    private void initialize() {
+        new Thread(() -> udpQueueProducer.produce()).start();
+        new Thread(() -> udpQueueConsumer.consume()).start();
+        new Thread(() -> udpProducerBroker.startBroker()).start();
+    }
+
+}
